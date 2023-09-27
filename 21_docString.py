37a33a774c0e090bb0a0ac07f7d7db6235e78790
Patel Priya <135525235+priya8926@users.noreply.github.com>
2023-09-27 23:28:33 +0530

    Delete 21_docString.py

--- 21_docString.py
+++
@@ -1,7 +0,0 @@
-# Python docstrings are the string literals that appear right after the definition of a function, method, class, or module.
-
-def square(n):
-    ''' take in number n , return the square n'''  # it is not a comment it is docString
-    print(n**2)
-square(5)
-print(square.__doc__)

1ed304cbe4d621d2dc261bf48b64c5eddd19cd88
Patel Priya <135525235+priya8926@users.noreply.github.com>
2023-10-24 13:59:42 +0530

    Add files via upload

---
+++ Python/35_virtual_environment.py
@@ -0,0 +1,3 @@
+# using diffrent diffrent environment or version of pandas
+import pandas as pd
+print(pd.__version__)
---
+++ Python/36_import.py
@@ -0,0 +1,9 @@
+# import math
+# from math import sqrt
+# result = math.sqrt(9)
+# result = sqrt(9)
+# print(result)
+
+# import math
+# # print(dir(math))
+# print(math.nan , type(math.nan))
